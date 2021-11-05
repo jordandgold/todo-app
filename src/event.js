1a2a3389965c79edea0e4e4d523f2feb0955f8b8
Jordan Gold <jordandgold@gmail.com>
2021-11-04 20:52:32 -0600

    add prettier and eslint

--- src/event.js
+++ src/event.js
@@ -1,15 +1,15 @@
 class Event {
     constructor() {
-      this.listeners = [];
+        this.listeners = [];
     }
         
-            addListener(listener) {
-            this.listeners.push(listener);
-            }
-        
-            trigger(params) {
-            this.listeners.forEach(listener => { listener(params); });
-            }
+    addListener(listener) {
+        this.listeners.push(listener);
+    }
+
+    trigger(params) {
+        this.listeners.forEach(listener => { listener(params); });
+    }
 }
   
 export default Event;
